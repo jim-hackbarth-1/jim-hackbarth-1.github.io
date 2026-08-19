993702b9f297d51e84549ec1db03a46c1a4fb894
jim-hackbarth-1 <164082662+jim-hackbarth-1@users.noreply.github.com>
2026-08-19 15:25:02 -0500

    Update map-worker-client.js

--- atlas-cooperative/domain/utilities/map-worker-client.js
+++ atlas-cooperative/domain/utilities/map-worker-client.js
@@ -29,6 +29,7 @@ export class MapWorkerClient {
 
     // methods
     static initializeWorker(appDocument, canvas, mapChangeListener, baseUrl) {
+        console.log('initializeWorker1');
         this.#currentCanvasSize = { height: canvas.height, width: canvas.width };
         MapWorkerClient.#addDocumentEventHandlers(appDocument);
         MapWorkerClient.#addCanvasEventHandlers(canvas);
@@ -42,6 +43,8 @@ export class MapWorkerClient {
             canvas: offscreenCanvas,
             baseUrl: baseUrl
         };
+        console.log('initializeWorker2');
+        console.log(message);
         MapWorkerClient.#mapWorker.postMessage(message, [offscreenCanvas, MapWorkerClient.#channel.port2]);
     }
 
